3fda060936e680b997c7248c98dcb97db55227ff
Malyshev Aleksandr <a.malyshev@ymail.com>
2022-08-17 01:15:40 +0300

    Task2

--- JavaMarathon2021/src/main/java/day1/Task2.java
+++ JavaMarathon2021/src/main/java/day1/Task2.java
@@ -2,6 +2,12 @@ package day1;
 
 public class Task2 {
     public static void main(String[] args) {
+//        2. Вывести на экран слово “JAVA”, в строку, чтобы оно повторилось 10 раз, используя цикл for.
+//        Вывод в консоль должен быть таким:
+//        JAVA JAVA JAVA JAVA JAVA JAVA JAVA JAVA JAVA JAVA
 
+        for (int i = 0; i <= 10; i++) {
+            System.out.print("Java ");
+        }
     }
 }
