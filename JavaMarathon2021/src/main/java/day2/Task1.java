a086b078694937f06667d76b7a30dcc9dd904283
Malyshev Aleksandr <a.malyshev@ymail.com>
2022-08-17 23:56:26 +0300

    Task1,2

--- JavaMarathon2021/src/main/java/day2/Task1.java
+++ JavaMarathon2021/src/main/java/day2/Task1.java
@@ -1,7 +1,20 @@
 package day2;
 
+import java.util.Scanner;
+
 public class Task1 {
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        int flour = scanner.nextInt();
+        System.out.println("Введите количество этажей " + flour );
+       if (flour >0 && flour <= 4){
+           System.out.println("Малоэтажный дом");
+       } else if (flour >=5  && flour <= 8) {
+           System.out.println("Среднеэтажный дом");
+       } else if (flour >= 9) {
+           System.out.println("Многоэтажный дом");
+       } else {
+           System.out.println( "Ошибка ввода");
+       }
     }
 }
--- JavaMarathon2021/src/main/java/day2/Task2.java
+++ JavaMarathon2021/src/main/java/day2/Task2.java
@@ -1,7 +1,18 @@
 package day2;
 
+import java.util.Scanner;
+
 public class Task2 {
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        System.out.println("Введите число а:" + a );
+        System.out.println("Введите число b:" + b );
+        for ( ; a < b; a++ ) {
+            if (a % 5 == 0 && a % 10 != 0) {
+                System.out.println(a);
+            }
+        }
     }
 }
