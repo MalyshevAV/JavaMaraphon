898f6cd77f939e955997c81192e712d5ede6f269
Malyshev Aleksandr <a.malyshev@ymail.com>
2022-08-29 10:06:31 +0300

    Task5

--- JavaMarathon2021/src/main/java/day4/Task4.java
+++ JavaMarathon2021/src/main/java/day4/Task4.java
@@ -1,7 +1,31 @@
 package day4;
 
+import java.util.Arrays;
+
 public class Task4 {
     public static void main(String[] args) {
+//        Создать новый массив размера 100 и заполнить его случайными числами из диапазона от 0 до 10000.
+//        Найти максимум среди сумм трех соседних элементов. Для найденной тройки с максимальной суммой
+//        выведите значение суммы и индекс первого элемента тройки.
+        int [] array = new int [100];
+        for (int i = 0; i < array.length; i++ )
+            array[i] = (int) (Math.random() * 10000 + 1);
+        System.out.println(Arrays.toString(array));
 
+        int maxSum = 0;
+        int maxSumIndex = 0;
+        int arrayLenght= 3;
+        for (int i = 0; i <= array.length - arrayLenght;  i++) {
+            int sum = 0;
+            for (int j = i; j < i + arrayLenght; j++) {
+                sum += array[j];
+            }
+            if (sum > maxSum) {
+                maxSum = sum;
+                maxSumIndex = i;
+            }
+        }
+        System.out.println(maxSumIndex);
+        System.out.println(maxSum);
     }
 }
